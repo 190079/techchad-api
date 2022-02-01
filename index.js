7da3ac601f05eb1df1fc4a3edff2f515a66f59a6
190079 <190079@softwarica.edu.np>
2022-02-01 21:08:08 +0545

    cors added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 const projects = require('./src/functions/Projects');
 const InitiateMongoServer = require('./src/mongodb/db');
 
 InitiateMongoServer();
 
 const app = express();
+app.use(cors({
+  origin: "*",
+}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const port =  1414;
 
 app.use(express.json());
@@ -16,8 +23,8 @@ app.get('/', (req, res) => {
   res.json({"test" : "successful"});
 });
 
-
 app.use("/projects",projects);
+app.use("/images",express.static('./src/public/images/'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
